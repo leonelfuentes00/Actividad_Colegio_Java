dfbe2dc7ce798aac47669ff507edc8c726c81725
Fuentes Leonel <128540751+leonelfuentes00@users.noreply.github.com>
2023-11-10 12:21:10 -0300

    primer commit

---
+++ Actividad_Colegio/src/Main.java
@@ -0,0 +1,9 @@
+import Formularios_Registro.Registro_Usuario;
+
+public class Main
+{
+     public static void main(String[] args) {
+        Registro_Usuario reg = new Registro_Usuario();
+        reg.setVisibleRegistrar();
+    }
+}
